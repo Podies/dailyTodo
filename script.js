dc41fdf2bba6996f5d12fefd37937d7ce866e43e
suraj kumar <ravi.suraj110@gmail.com>
2016-10-14 23:43:15 +0530

    Add new todo, calculate percentage

--- script.js
+++ script.js
@@ -44,20 +44,76 @@ function getTodayDate () {
   return formatDate;
 }
 
+function generateTodoHTML(todo){
+  var status = todo.status ? "checked" : "";
+  return (
+    `<div class="col-xs-7 col-sm-6 col-md-6 col-lg-6">
+      <label class="label-control">
+        Event
+      </label>
+      <input type="text" name="" id="input" value="${todo.name}" data-name="name" data-id="${todo.id}" onchange="updateStorage(event)" class="form-control">
+    </div>
+    <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+      <label class="label-control">
+        From
+      </label>
+      <input type="text" name="" id="input" value="${todo.from}" data-name="from" data-id="${todo.id}"  onchange="updateStorage()" class="form-control">
+    </div>
+    <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+      <label class="label-control">
+        To
+      </label>
+      <input type="text" name="" id="input" value="${todo.to}" data-name="to" data-id="${todo.id}" onchange="updateStorage()" class="form-control">
+    </div>
+    <div class="col-xs-12 col-sm-1 col-md-1 col-lg-1">
+      <div class="checkbox text-right">
+          <input type="checkbox" data-id="${todo.id}" onclick="toggleStatus(event)" ${status}>
+      </div>
+    </div>`
+  )
+}
+
+// [{"date":"14-10-2016","todos":[{"id":1,"name":"Brink Milk ","from":"","to":"","status":false}],"counter":1}]
+//Update Storage
+function updateStorage(e) {
+  var localData = JSON.parse(localStorage.getItem("dailyEntry"));
+  var selectedId = e.currentTarget.getAttribute('data-id');
+  var selectedElement = e.currentTarget.getAttribute('data-name');
+  var value = e.currentTarget.value;
+  // Filter Todays Todo
+  var todaysTodo = localData.filter(function(todo) {
+    return todo.date == getTodayDate();
+  });
+  //Get Current todo and Update
+  var index;
+  todaysTodo[0].todos.forEach(function(todo, i){
+    if(todo.id == selectedId){
+      index = i;
+    }
+  });
+  // Find todo matching current id
+  // update name/from/to of that todo
+  localData[0]['todos'][index][selectedElement] = value;
+
+  // Set localstorage
+  localStorage.setItem("dailyEntry", JSON.stringify(localData));
+}
+
 function buildDom() {
   var localData = JSON.parse(localStorage.getItem("dailyEntry"));
   var formatDate = getTodayDate();
   var filteredTodos = localData.filter(function(item){
     return item.date === formatDate;
   });
+  var generatedHTML = "";
 
   filteredTodos[0].todos.forEach(function(todo, i){
-    var parentDiv = document.createElement("div");
-    parentDiv.appendChild(document.createElement("input")); 
-    parentDiv.appendChild(document.createElement("input")); 
-    parentDiv.appendChild(document.createElement("input")); 
-    
+    generatedHTML += generateTodoHTML(todo);
   });
+  document.getElementById('todo-list').innerHTML = generatedHTML;
+
+  //
+  calculatePercentage();
 }
 
 
@@ -74,7 +130,7 @@ function renderTodo(){
     if(filteredTodos.length == 0) {
       // Append todays todos
       localData.unshift({date: formatDate, todos: [{id: 1, name: '', from: '', to: '', status: false}], counter: 1 });
-      localStorage.setItem('dailyEntry', localData);
+      localStorage.setItem('dailyEntry', JSON.stringify(localData));
     }
 
   } else {
@@ -83,6 +139,48 @@ function renderTodo(){
 
   // buildDom
   buildDom();
+}
+function toggleStatus(e){
+  var localData = JSON.parse(localStorage.getItem('dailyEntry'));
+  var selectedId = e.currentTarget.getAttribute('data-id');
+
+  localData[0].todos.forEach(function(todo, i){
+    if(todo.id == selectedId) {
+      todo.status = todo.status ? false : true;
+    }
+  });
+
+  localStorage.setItem('dailyEntry', JSON.stringify(localData));
+  buildDom();
+}
+
+function addNewTodo(){
+ var localData = JSON.parse(localStorage.getItem("dailyEntry"));
+ var currentCounter = localData[0].counter;
+ localData[0].todos.push({id: currentCounter + 1, name: '', from: '', to: '', status: false});
+ localData[0].counter = currentCounter + 1;
+ localStorage.setItem('dailyEntry', JSON.stringify(localData));
+ buildDom();
+}
+
+function calculatePercentage(){
+  var localData = JSON.parse(localStorage.getItem("dailyEntry"));
+  var totalTodos = localData[0].todos.length;
+  var completedTodos = localData[0].todos.filter(function(todo){
+    return todo.status == true;
+  });
+
+  completedTodos = completedTodos.length;
+
+  var percentage = Math.ceil((completedTodos/totalTodos)*100);
+  document.getElementById('percentage').innerHTML = percentage;
+
+  if(percentage > 50){
+    document.getElementById('left-half').style.transform = "rotate(180deg)";
+    var rightPercentage = (percentage - 50)*3.6;
+    document.getElementById('right-half').style.transform = "rotate(90deg)";
+  } 
+
 }
 
 renderTodo();
